7d29c2634aa22b4ea65e36d3ff25e8bbef2c8db9
diegovinha <diegovinha57@gmail.com>
2026-09-22 04:39:36 +0000

    Atualiza código para web e adiciona imagem de fundo

--- app_curva5051.py
+++ app_curva5051.py
@@ -2155,7 +2155,7 @@ for idx, p in enumerate(st.session_state.pontos_ensaio):
 
   with col_r1:
     #st.text(p["id"])
-    st.markdown(f"<span style='color:#ffffff !important;'>{p['id']}</span>", unsafe_allow_html=True)
+    st.markdown(f'<span style="color:#ffffff !important;">{p['id']}</span>', unsafe_allow_html=True)
   with col_r2:
     iprim_val = st.number_input(
         f"Primária {idx}",
@@ -2168,7 +2168,7 @@ for idx, p in enumerate(st.session_state.pontos_ensaio):
   with col_r3:
     i_sec = iprim_val / rel_tc_Ativo
     #st.text(f"{i_sec:.2f} A")
-    st.markdown(    f'<span style="color:#ffffff !important; font-size:18px; font-weight:700;">{i_sec:.2f} A</span>', unsafe_allow_html=True)
+    st.markdown(f'<span style="color:#ffffff !important; font-size:18px; font-weight:700;">{i_sec:.2f} A</span>', unsafe_allow_html=True)
   with col_r4:
     t_teorico = calcular_teorico_geral(iprim_val, n_tipo_Ativo, c_tipo_Ativo, p_51_Ativo, tms_Ativo, p_50_Ativo, t_inst_Ativo)
     if pd.isna(t_teorico) or t_teorico == 0:
