6f01ae1715ec59208d81926125dc22bdd1fdd805
diegovinha <diegovinha57@gmail.com>
2026-09-22 05:24:46 +0000

    Atualiza código para web e adiciona imagem de fundo

--- app_curva5051.py
+++ app_curva5051.py
@@ -2155,7 +2155,7 @@ for idx, p in enumerate(st.session_state.pontos_ensaio):
 
   with col_r1:
     #st.text(p["id"])
-    st.markdown(f"<div class='titulo-tabela''>{p['id']}</div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='titulo-tabela'>{p['id']}</div>", unsafe_allow_html=True)
   with col_r2:
     iprim_val = st.number_input(
         f"Primária {idx}",
@@ -2168,7 +2168,7 @@ for idx, p in enumerate(st.session_state.pontos_ensaio):
   with col_r3:
     i_sec = iprim_val / rel_tc_Ativo
     #st.text(f"{i_sec:.2f} A")
-    st.markdown(f'<span style="color:#ffffff !important; font-size:18px; font-weight:700;">{i_sec:.2f} A</span>', unsafe_allow_html=True)
+    st.markdown(f'<div class="titulo-tabela">{i_sec:.2f} A</span>', unsafe_allow_html=True)
   with col_r4:
     t_teorico = calcular_teorico_geral(iprim_val, n_tipo_Ativo, c_tipo_Ativo, p_51_Ativo, tms_Ativo, p_50_Ativo, t_inst_Ativo)
     if pd.isna(t_teorico) or t_teorico == 0:
@@ -2176,7 +2176,7 @@ for idx, p in enumerate(st.session_state.pontos_ensaio):
     else:
       t_teorico_str = f"{t_teorico:.3f} s"
     #st.text(t_teorico_str)
-    st.markdown(f'<span style="color:#ffffff !important; font-size:18px; font-weight:700;">{t_teorico_str}</span>', unsafe_allow_html=True)
+    st.markdown(f'<div class="titulo-tabela">{t_teorico_str}</span>', unsafe_allow_html=True)
 
 
   with col_r5:
